74ef787df6f0073a848e95edeefd8ddab0d9f1e3
RaviPrakashChoudhary <154719029+RaviPrakashChoudhary@users.noreply.github.com>
2024-02-02 06:51:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let hrs = document.querySelector("#hrs");
+let min = document.querySelector("#min");
+let sec = document.querySelector("#sec");
+let date = document.querySelector(".date");
+
+setInterval(()=>{
+    let currentTime = new Date();
+    let todayDate = currentTime.getDate() + "-" + currentTime.getMonth() + "-" + currentTime.getFullYear();
+    console.log ()
+    date.innerHTML = todayDate;
+     hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+     min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+     sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+},100)
+
